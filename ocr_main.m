bc009e7c698799a64573d5335639b2fab088eda5
englandmr <englandmr@englandmrs-MacBook-Pro.local>
2016-05-05 21:54:05 -0500

    adding main script

---
+++ ocr_main.m
@@ -0,0 +1,13 @@
+
+% Load database of 1 - 9 HOG from file or do in memory
+
+% Run image through entropy maximization code
+
+% Generate HOG based on the invariant image
+
+% Create pmf of HOG
+
+% run pmf through databse
+
+% report class found
+
