2992a066ce2c16c87ca5b5053a7bedcfc149395e
englandmr <englandmr@englandmrs-MacBook-Pro.local>
2016-05-05 21:57:29 -0500

    adding main code logic

--- ocr_main.m
+++ ocr_main.m
@@ -1,13 +1,13 @@
 
 % Load database of 1 - 9 HOG from file or do in memory
-
+HOG_Database = load_HOG_database(); % not implemented
 % Run image through entropy maximization code
 
 % Generate HOG based on the invariant image
-
+HOG = generate_HOG(invariant_img); % not implemented
 % Create pmf of HOG
-
+pmf_hog = generate_pmf(HOG);
 % run pmf through databse
-
+class = classify_digit(pmf_hog,HOG_Database); % not implemented
 % report class found
 
