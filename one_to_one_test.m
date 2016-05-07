533f3b096b5921ebe90213d0e19b279aeaec49ad
englandmr <englandmr@mwc-120171.dhcp.missouri.edu>
2016-05-07 12:51:35 -0500

    good tester for base

--- one_to_one_test.m
+++ one_to_one_test.m
@@ -3,10 +3,11 @@ nbins = 360/10;
 % Load database of 1 - 9 HOG from file or do in memory
 [HOG_Database] = load_HOG_database(nbins);
 % input image for classification
-I = imread('test_data_digits/1_shadow_red.png'); % read an image
-    
+I = imread('database_digits/4.png'); % read an image
+bwI = im2bw(I, 0.784);
+bwI = bwI .* 255;
 % Generate HOG based on the invariant image
-HOG = generate_HOG(,nbins); % not implemented
+HOG = generate_HOG(bwI,nbins); % not implemented
 % Create pmf of HOG
 pmf_hog = generate_pmf(HOG);
 % run pmf through databse
