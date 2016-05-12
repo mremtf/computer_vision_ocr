98ee19129c7cbef898a8e08fc292b6875b93a6fd
englandmr <englandmr@mwc-121058.dhcp.missouri.edu>
2016-05-12 10:21:22 -0500

    done

--- ocr_main.m
+++ ocr_main.m
@@ -3,23 +3,33 @@ nbins = 360/10;
 % Load database of 1 - 9 HOG from file or do in memory
 [HOG_Database] = load_HOG_database(nbins);
 % input image for classification
-I = imread('test_data_digits/3_shadow.png'); % read an image
-    I = preprocess(I);
-
+I = imread('test_data_digits/9_shadow_red.png'); % read an image
+% apply preprocess stage of bluring
+I = preprocess(I);
+figure; imshow(I);
 % Run image through entropy maximization code
-[Invariant,~] = gfinvim(I,'entropy','shannon','demo',true);
+[Invariant,~] = gfinvim(I,'entropy','shannon','demo',false);
 figure;imshow(Invariant); 
+% binarization of invariant image
 Ivariant2 = im2uint8(Invariant);
 Ivariant2 = im2bw(Ivariant2, 0.3);
-%I2 = imread('database_digits/1.png');
-%I2 = im2bw(rgb2gray(I2),.784);
 Ivariant2 = Ivariant2 .* 255;
+% apply postprocess stage dialation
+Ivariant2 = postprocess( Ivariant2 ); 
 % Generate HOG based on the invariant image
-Ivariant2 = postprocess( Ivariant2 );   
 figure;imshow(Ivariant2); 
-HOG = generate_HOG(Ivariant2,nbins); % not implemented
+HOG = generate_HOG(Ivariant2,nbins);
 % Create pmf of HOG
 pmf_hog = generate_pmf(HOG);
+%figure; hold on; bar(pmf_hog,'r'); bar(HOG_Database(1,:)); 
+%legend('Shadow Free PMF','Database PMF of 1');
+%title('Shadow Free PMF verse Databse PMF of 1');
+%figure; hold on; bar(pmf_hog,'r'); bar(HOG_Database(3,:));
+%legend('Shadow Free PMF','Database PMF of 3');
+%title('Shadow Free PMF verse Databse PMF of 3');
+%figure; hold on; bar(pmf_hog,'r'); bar(HOG_Database(5,:));
+%legend('Shadow Free PMF','Database PMF of 5');
+%title('Shadow Free PMF verse Databse PMF of 5');
 % run pmf through databse
 espilion = 14.0;
 class = classify_digit(pmf_hog,HOG_Database,espilion); 
